5c6c88590753d57b999bca99581701b7412ede0c
root <root@Chacha.>
2024-12-11 20:26:31 +0100

    Create the code that insert sort the list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,44 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
  *insertion_sort_list - function that sorts an array of integers in ascending
  *@list: double pointer struct
  *Return: Void
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *buff, *current, *next, *prev;
+	listint_t *buff, *current, *prev;
 
-	if (!list || !(*list)->next)
-	{
+	if (!list || !(*list) || !(*list)->next)
 		return;
-	}
 	current = (*list)->next;
-	
 	while (current)
 	{
 		buff = current;
 		prev = current->prev;
-		next = current->next;
-		printf("cc %d %d\n", buff->n, prev->n);
-		
 		while (prev && prev->n > current->n)
 		{
-			next = prev;
-			prev = prev->prev;
-			printf("cc2 %d %d\n", buff->n, prev->n);
+			if (prev->prev)
+			{
+				prev->prev->next = buff;
+				buff->prev = prev->prev;
+			}
+			else
+			{
+				buff->prev = NULL;
+				*list = buff;
+			}
+			if (buff->next)
+			{
+				prev->next = buff->next;
+				prev->next->prev = prev;
+			}
+			else
+				prev->next = NULL;
+			prev->prev = buff;
+			buff->next = prev;
+			print_list(*list);
+			prev = buff->prev;
 		}
-		prev->next = buff;
-		buff->next = next;
 		current = current->next;
-		printf("cc3\n");
-		print_list(*list);
-		printf("cc4\n");
 	}
-
 }
